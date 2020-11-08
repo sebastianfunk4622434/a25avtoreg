1c55dad22d9fdb1e667129aa37a137e696a66c18
merlebeer1349686 <71511049+merlebeer1349686@users.noreply.github.com>
2020-11-09 01:58:43 +0400

    1

--- organic-teatox_integration/transit/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('[data-href]').on('click', function() {
-        $('html, body').animate({
-            scrollTop: $($(this).data('href')).offset().top
-        }, 800)
-    })
-})
